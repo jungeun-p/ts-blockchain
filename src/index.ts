c088d22c04c7dbffc89d64d42fb9d4077e7f67b0
jungeun-p <mrk7753@naver.com>
2022-08-17 20:11:24 +0900

    [ modify ] Add Block Class

--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,36 @@ class Block implements BlockShape {
         return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class BlockChain {
+    private blocks: Block[]
+    constructor(){
+        // blocks 초기화
+        this.blocks = [];
+    }
+    // 2. 이전 hash 가져오기
+    private getPrevHash(){
+        if(this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length - 1].hash;
+
+    }
+    // 1. blcok에 저장할 데이터를 전달 
+    public addBlock(data: string){
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length+1, data);
+        this.blocks.push(newBlock);
+    }
+    public getBlocks(){
+        // return this.blocks;
+        return [...this.blocks]; // 해당 데이터를 가진 새로운 배열 return
+    }
+}
+
+const blockchain = new BlockChain();
+blockchain.addBlock("first one");
+blockchain.addBlock("second one");
+blockchain.addBlock("third one");
+blockchain.addBlock("Fourth one");
+
+blockchain.getBlocks().push(new Block("...", 11111, "HACK"));
+
+console.log(blockchain.getBlocks());
