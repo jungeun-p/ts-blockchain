c743cc97125492584c7dbbfd9ab55a9d0edd67ec
jungeun-p <mrk7753@naver.com>
2022-08-17 15:52:02 +0900

    [ modify ] Declaration JS file with d.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-class Block {
-    constructor(
-        private data: string
-    ){}
-    static hello(){
-        return "hello";
-    }
-}
+// class Block {
+//     constructor(
+//         private data: string
+//     ){}
+//     static hello(){
+//         return "hello";
+//     }
+// }
+
+import { init, exit } from "myPackage";
+
+init({
+    url: "true"
+})
+
+exit(1)
