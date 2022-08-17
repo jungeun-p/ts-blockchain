cc4e018d28d1cba1c65bf472e95d5ba9da07bef7
jungeun-p <mrk7753@naver.com>
2022-08-17 17:15:38 +0900

    [ modify ] Make a Block with ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
-// class Block {
-//     constructor(
-//         private data: string
-//     ){}
-//     static hello(){
-//         return "hello";
-//     }
-// }
+import crypto from "crypto";
 
-import { init, exit } from "myPackage";
-init({
-    url:"hello",
-})
-exit(1)
+interface BlockShape {
+    hash: string; // prevHash, height, data를 이용한 계산값 
+    prevHash: string;
+    height: number;
+    data: string;
+}
+
+class Block implements BlockShape {
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string,
+    ){
+        // hash 초기화
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    // static 함수 
+    static calculateHash(prevHash: string, height: number, data: string){
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
+    }
+}
